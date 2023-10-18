0aa10058d7f80d8a7a4594f08557f46c4ea02785
rupesh.raut <rupesh.raut@amniltech.com>
2023-10-18 23:08:43 +0545

    feat: :fire: ADvanced properties of set
    got familiar to advanced sets such as union, difference, intersection

---
+++ 06_advanced-sets/code.py
@@ -0,0 +1,25 @@
+s = {"Apple","Orange", "Mango"}
+
+my_set = set(["Apple","Orange","Mango"])
+
+# my_set.pop()
+# my_set.remove("Mango")
+# print(my_set)
+
+
+cities = {"Lalitpur", "Pokhara", "Kathmandu"}
+
+cities_2 = {"Hetauda", "Pokhara", "Birgunj"}
+
+# only one shown in union if more then 1 present in set
+# rupesh = cities.union(cities_2)
+
+# only the common set element is displayed in intersection
+# rupesh = cities.intersection(cities_2)
+
+#  present in current but not in compared one - difference
+# rupesh = cities.difference(cities_2)
+# rupesh = cities_2.difference(cities)
+
+
+print(rupesh)
