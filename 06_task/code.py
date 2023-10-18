477cafa189e98c829cc58ae9cbdb69a02fa4bbf3
Rupesh Raut <iamrupeshraut1@gmail.com>
2023-10-18 23:21:48 +0545

    feat: :fire: tuple, list and set simple exercise

---
+++ 06_task/code.py
@@ -0,0 +1,21 @@
+# List, Tuples, and set
+# 1. Create a list, called eg_list, with three numbers. The total of the numbers added together should be 99.
+# 2. Create a tuple, called eg_tuple, with a two value in it.
+# 3. Modify the variable s2 so that s1.intersection(s2) returns {22,88,9,12}
+
+
+
+#1 task
+eg_list = [10,80,9]
+
+#2 task
+eg_tuple = ("Ramesh", "Suresh")
+
+#3 intersection
+
+s1 = {12, 2, 22, 88, 9, 12}
+s2 = {43,23,22,88,9,12}
+
+task3 = s1.intersection(s2)
+
+# print(eg_list, eg_tuple, task3)s
